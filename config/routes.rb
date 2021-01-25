d8d44cca538ee65c4658dbfdc0d8b8d78e7432d8
Mikey Esteban <michaelp.esteban@gmail.com>
2021-01-25 10:07:29 -0500

    added Devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :chirps, param: :slug
+      resources :chirps
       # resources :comments, only: [ :create, :destroy ]
     end
   end
